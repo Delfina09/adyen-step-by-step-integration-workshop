01a0e583aed4c69f3c3bcfdec3e0d670d594c01a
Delfina09 <delfina.gerardo@wallia.io>
2026-03-12 17:03:19 +0000

    update test

--- src/main/resources/static/adyenWebImplementation.js
+++ src/main/resources/static/adyenWebImplementation.js
@@ -1,11 +1,11 @@
-const clientKey = document.getElementById("clientKey").innerHTML;
+const clientKey = document.getElementById("test_OHFC232L7ZH5JFVCFEMDHBHBYMD7BDBE").innerHTML;
 const { AdyenCheckout, Dropin } = window.AdyenWeb;
 
 // Starts the (Adyen.Web) AdyenCheckout with your specified configuration by calling the `/paymentMethods` endpoint.
 async function startCheckout() {
     try {
         // Step 8 - Retrieve the available payment methods
-
+        <div id="clientKey" class="hidden" th:text="${clientKey}"></div>
     } catch (error) {
         console.error(error);
         alert("Error occurred. Look at console for details.");
